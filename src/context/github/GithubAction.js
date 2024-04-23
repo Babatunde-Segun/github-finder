e06c4dea3d5a731b6c65fcd617dc6f07f187e7a7
Babatunde-Segun <officiallyvincify@gmail.com>
2024-04-23 11:04:11 +0100

    using axios straight up

--- src/context/github/GithubAction.js
+++ src/context/github/GithubAction.js
@@ -10,15 +10,40 @@ const github = axios.create({
   },
 });
 
+const config = {
+  headers: {
+    Authorization: `Token ${GITHUB_TOKEN}`,
+  },
+};
+
 // Get search users (testing propose)
 export const searchUser = async (text) => {
   // const params = new URLSearchParams({
   //   q: text,
   // });
 
+  // const config = {
+  //   headers: {
+  //     Authorization: `Token ${GITHUB_TOKEN}`,
+  //   },
+  // };
+
+  // const response1 = await fetch(API_URL, {
+  //   method: "GET",
+  //   headers: { "Content-Type": "application/json" },
+  // });
+
+  const response2 = await axios.get(
+    `${GITHUB_URL}/search/users?q=${text}`,
+
+    config
+  );
+
+  console.log(response2);
+
   const response = await github.get(`/search/users?q=${text}`);
 
-  return response.data.items;
+  return response2.data.items;
 };
 
 // Get a single user and repos
@@ -29,10 +54,25 @@ export const getUserAndRepos = async (login) => {
   //   per_page: 10,
   // });
 
+  const response1 = await axios.get(
+    `${GITHUB_URL}/users/${login}`,
+
+    config
+  );
+
+  const response2 = await axios.get(
+    `${GITHUB_URL}/users/${login}/repos?sort=created&per_page=10`,
+
+    config
+  );
+
   const [user, repos] = await Promise.all([
     github.get(`/users/${login}`),
     github.get(`/users/${login}/repos?sort=created&per_page=10`),
   ]);
 
-  return { user: user.data, repos: repos.data };
+  const [user1, repos1] = await Promise.all([response1, response2]);
+  console.log(user1);
+  console.log(repos1);
+  return { user: user1.data, repos: repos1.data };
 };
